70b2335888225a0d36bfec61da446eb8069ca2e8
busrasariguzel <busra.sariguzel@codeimmersives.com>
2020-04-16 15:48:33 -0400

    created a journal routes and got the functionality

--- routes/journalRoutes.js
+++ routes/journalRoutes.js
@@ -1,9 +1,60 @@
 const express = require('express');
 const router = express.Router();
+const Journal = require('./models/Journal')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.post('/journal', (req, res) => {
+  const newJournal = new Journal();
+  newJournal.date = new Date().toDateString();
+  newJournal.entry1 = req.body.entry1;
+  newJournal.entry2 = req.body.entry2;
+  newJournal.entry3 = req.body.entry3;
+  newJournal.entry4 = req.body.entry4;
+  newJournal.entry5 = req.body.entry5;
+  newJournal.save()
+  .then((journal) => {
+    return res.status(200).json({ message: 'Success', journal });
+  }).catch(err => res.status(500).json(err));
+})
+
+
+router.get('/journals', (req,res)=> {
+    Journal.find({}).then((journals) => {
+      journals.reverse();
+      return res.status(200).json({ journals });
+    }).catch(err=>res.status(500).json({message: 'Server Error', err}))
 });
+
+
+router.get('/journal/:id', (req, res) => {
+  Journal.findById({ _id: req.params.id })
+  .then((journal) => {
+    return res.status(200).json({ journal });
+  }).catch(err=>res.status(500).json({message: ' Error', err}))
+});
+
+router.put('/journal/:id', (req, res) => {
+  Journal.findById({ _id: req.params.id }).then((journal) => {
+
+    journal.entry1 = req.body.entry1 ? req.body.entry1 : journal.entry1;
+    journal.entry2 = req.body.entry2 ? req.body.entry2 : journal.entry2;
+    journal.entry3 = req.body.entry3 ? req.body.entry3 : journal.entry3;
+    journal.entry4 = req.body.entry4 ? req.body.entry4 : journal.entry4;
+    journal.entry5 = req.body.entry5 ? req.body.entry5 : journal.entry5;
+    
+    journal.save().then((journal) => {
+      return res.status(200).json({ message: 'Journal is updated', journal });
+    }).catch(err=>res.status(500).json({message: ' Error', err}))
+  });
+})
+
+router.delete('/journal/:id', (req,res)=> {
+  Journal.findByIdAndDelete({ _id: req.params.id })
+  .then(
+  res.json({ message: 'Journal is deleted' })
+  );
+})
+
+
 
 module.exports = router;
--- routes/models/Journal.js
+++ routes/models/Journal.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
 const JournalSchema = new mongoose.Schema({
-    Date: { type: String, default: '', trim:true },
-    Entry1: { type: String, default: '', trim:true },
-    Entry2: { type: String, default: '', trim:true },
-    Entry3: { type: String, default: '', trim:true },
-    Entry4: { type: String, default: '', trim:true },
-    Entry5: { type: String, default: '', trim:true },
+    date: { type: String, default: '', trim:true },
+    entry1: { type: String, default: '', trim:true },
+    entry2: { type: String, default: '', trim:true },
+    entry3: { type: String, default: '', trim:true },
+    entry4: { type: String, default: '', trim:true },
+    entry5: { type: String, default: '', trim:true },
 
 });
 
